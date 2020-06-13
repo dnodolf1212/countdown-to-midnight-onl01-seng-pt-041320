ecf40a7721bb5b052081103e109218ff4078e311
Dwight Nodolf <dnodolf@gmail.com>
2020-06-13 03:11:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+  number -= 1
+end
+  if number + 12 == 12
+    return "HAPPY NEW YEAR!"
+end
+end
+
+def countdown_with_sleep(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+  number -= 1 
+  sleep(5)
+end
 end
